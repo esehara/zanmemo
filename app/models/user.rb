7a6ed5b43440f385de07739899cb32f08b366058
esehara shigeo <esehara@gmail.com>
2014-10-10 18:55:25 +0900

    add: login service with omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      useer.trace_id = auth["info"]["nickname"]
+    end
+  end
 end
