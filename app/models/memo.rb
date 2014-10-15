4fadede2886d501ca3ffa2d3c162360d0bed065b
esehara shigeo <esehara@gmail.com>
2014-10-16 01:49:38 +0900

    bug fix: modify url itself !!

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -4,8 +4,10 @@ class Memo < ActiveRecord::Base
   validates :content, length: {:maximum => 3400}
 
   before_save do |memo|
-    trace_id = Digest::SHA1.hexdigest(Time.now.to_s)
-    memo.trace_id = trace_id.to_s
+    if memo.trace_id == nil
+      trace_id = Digest::SHA1.hexdigest(Time.now.to_s)
+      memo.trace_id = trace_id.to_s
+    end
   end
   
   belongs_to :user
